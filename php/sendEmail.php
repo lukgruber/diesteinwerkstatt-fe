89f675bd253e987ff15745bbd2bc2b4406a14488
Lukas Gruber <31432319+lukgruber@users.noreply.github.com>
2022-08-01 19:14:04 +0200

    Update sendEmail.php

--- php/sendEmail.php
+++ php/sendEmail.php
@@ -3,7 +3,13 @@
 // Replace this with your own email address
 $to = 'office@diesteinwerkstatt-fe.at';
 
-
+function url(){
+  return sprintf(
+    "%s://%s",
+    isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off' ? 'https' : 'http',
+    $_SERVER['SERVER_NAME']
+  );
+}
 
 if($_POST) {
 
